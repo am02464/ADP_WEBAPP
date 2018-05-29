d0685cb66edbb29a1c9e5d1dc3b411598c05a01c
Atif Mehmood <35393156+am02464@users.noreply.github.com>
2018-05-29 16:35:47 +0500

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -82,6 +82,12 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 DATABASES['default'].update(db_from_env)
 
 
