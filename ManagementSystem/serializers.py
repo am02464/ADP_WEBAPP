a55f62fcf0f3fa71c947eaf96bf2b03fff68a57f
Atif Mehmood <35393156+am02464@users.noreply.github.com>
2018-06-22 16:01:07 +0500

    Update serializers.py

--- ManagementSystem/serializers.py
+++ ManagementSystem/serializers.py
@@ -1,8 +1,1 @@
-from rest_framework import serializers
-from .models import Students
 
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Students
-        fields = '__all__' 
